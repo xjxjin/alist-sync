cddf0c586e8ad3ede1550fe21f04cbfb4a75e990
xiaojin <390540332@163.com>
2024-05-10 03:17:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,14 +3,10 @@
 # start.sh
 
 # 检查是否提供了必要的环境变量
-if [ -z "$BASE_URL" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ] || [ -z "$DIR_PAIRS" ]; then
+if [ -z "$BASE_URL" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ] || [ -z "$DIR_PAIRS" ] || [ -z "$CRON_SCHEDULE" ]; then
     echo "Error: Missing one of the required environment variables."
     exit 1
 fi
 
 # 使用环境变量CRON_SCHEDULE创建crontab条目
-CRON_LINE="$CRON_SCHEDULE /usr/local/bin/python3 /app/alist-sync-test.py --base_url \"$BASE_URL\" --username \"$USERNAME\" --password \"$PASSWORD\" --dir_pairs \"$DIR_PAIRS\""
-echo $CRON_LINE | crontab -
-
-# 启动cron服务
-cron -f
+/usr/local/bin/python3 /app/alist-sync-test.py --base_url \"$BASE_URL\" --username \"$USERNAME\" --password \"$PASSWORD\" --dir_pairs \"$DIR_PAIRS\" --cron_schedule \"$CRON_SCHEDULE\"
