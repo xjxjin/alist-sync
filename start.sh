d7539a8a27d811621306f7099913ec36a40fb305
xiaojin <390540332@163.com>
2024-05-09 23:02:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [ -z "$BASE_URL" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ] || [ -z "$DIR_
 fi
 
 # 使用环境变量CRON_SCHEDULE创建crontab条目
-CRON_LINE="$CRON_SCHEDULE /usr/local/bin/python3 /app/alist-sync.py --base_url \"$BASE_URL\" --username \"$USERNAME\" --password \"$PASSWORD\" --dir_pairs \"$DIR_PAIRS\""
+CRON_LINE="$CRON_SCHEDULE /usr/local/bin/python3 /app/alist-sync-test.py --base_url \"$BASE_URL\" --username \"$USERNAME\" --password \"$PASSWORD\" --dir_pairs \"$DIR_PAIRS\""
 echo $CRON_LINE | crontab -
 
 # 启动cron服务
