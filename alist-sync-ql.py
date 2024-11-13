b613a7e942c3377d72f7d5775250b510718f12ce
xiaojin <390540332@163.com>
2024-11-13 15:44:05 +0800

    修复多目录中一个错误全部失败问题

--- alist-sync-ql.py
+++ alist-sync-ql.py
@@ -5,13 +5,14 @@ from datetime import datetime
 import os
 
 # 解析命令行参数
-base_url = os.environ.get('BASE_URL')
-username = os.environ.get('USERNAME')
-password = os.environ.get('PASSWORD')
-cron_schedule = os.environ.get('CRON_SCHEDULE')
 
-sync_delete_action = os.environ.get('SYNC_DELETE_ACTION', 'none').lower()
-sync_delete = sync_delete_action == 'move' or sync_delete_action == 'delete'
+base_url = os.environ.get("BASE_URL")
+username = os.environ.get("USERNAME")
+password = os.environ.get("PASSWORD")
+cron_schedule = os.environ.get("CRON_SCHEDULE")
+
+sync_delete_action = os.environ.get("SYNC_DELETE_ACTION", "none").lower()
+sync_delete = sync_delete_action == "move" or sync_delete_action == "delete"
 
 
 def xiaojin():
@@ -55,7 +56,7 @@ def get_dir_pairs_from_env():
     dir_pairs_list = []
 
     # 尝试从环境变量中获取DIR_PAIRS的值
-    dir_pairs = os.environ.get('DIR_PAIRS')
+    dir_pairs = os.environ.get("DIR_PAIRS")
 
     # 检查DIR_PAIRS是否不为空
     # print("本次同步目录有：")
@@ -67,7 +68,7 @@ def get_dir_pairs_from_env():
     # 循环尝试获取DIR_PAIRS1到DIR_PAIRS50的值
     for i in range(1, 51):
         # 构造环境变量名
-        env_var_name = f'DIR_PAIRS{i}'
+        env_var_name = f"DIR_PAIRS{i}"
         # 尝试获取环境变量的值
         env_var_value = os.environ.get(env_var_name)
         # 如果环境变量的值不为空，则添加到列表中
@@ -80,13 +81,13 @@ def get_dir_pairs_from_env():
 
 def create_connection(base_url):
     # 使用正则表达式解析URL，获取主机名和端口
-    match = re.match(r'(?:http[s]?://)?([^:/]+)(?::(\d+))?', base_url)
+    match = re.match(r"(?:http[s]?://)?([^:/]+)(?::(\d+))?", base_url)
     host = match.group(1)
     port_part = match.group(2)
-    port = int(port_part) if port_part else (80 if 'http://' in base_url else 443)
+    port = int(port_part) if port_part else (80 if "http://" in base_url else 443)
 
     # 根据URL的协议类型创建HTTP或HTTPS连接
-    return http.client.HTTPSConnection(host, port) if base_url.startswith('https://') else http.client.HTTPConnection(
+    return http.client.HTTPSConnection(host, port) if base_url.startswith("https://") else http.client.HTTPConnection(
         host, port)
 
 
@@ -105,8 +106,8 @@ def get_token(connection, path, username, password):
     # 获取认证token
     payload = json.dumps({"username": username, "password": password})
     headers = {
-        'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
-        'Content-Type': 'application/json'
+        "User-Agent": "Apifox/1.0.0 (https://apifox.com)",
+        "Content-Type": "application/json"
     }
     response = make_request(connection, "POST", path, headers, payload)
     return response["data"]["token"] if response else None
@@ -116,9 +117,9 @@ def directory_operation(connection, token, operation, **kwargs):
     # (connection, token, "mkdir", directory_path, path=directory_path)
     # 一个通用函数，用于执行目录操作
     headers = {
-        'Authorization': token,
-        'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
-        'Content-Type': 'application/json'
+        "Authorization": token,
+        "User-Agent": "Apifox/1.0.0 (https://apifox.com)",
+        "Content-Type": "application/json"
     }
     payload = json.dumps(kwargs)
     path = f"/api/fs/{operation}"  # 构建API路径
@@ -171,9 +172,9 @@ def directory_remove(connection, token, directory_path, file_name):
 def get_storage_list(connection, token):
     # 列出存储列表
     headers = {
-        'Authorization': token,
-        'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
-        'Content-Type': 'application/json'
+        "Authorization": token,
+        "User-Agent": "Apifox/1.0.0 (https://apifox.com)",
+        "Content-Type": "application/json"
     }
     response = make_request(connection, "GET", "/api/admin/storage/list", headers)
     storage_list = response["data"]["content"]
@@ -218,7 +219,7 @@ def recursive_copy(src_dir, dst_dir, connection, token, sync_delete=False):
 
                 if item_name in diff_list:
                     # 如果是移动判断源文件夹是否存在
-                    if sync_delete_action == 'move':
+                    if sync_delete_action == "move":
                         # 拼接移动文件路径
                         # 拼接到目标目录的trash下，获取文件夹所在的存储路径名称,拼接移动文件路径
                         trash_dir = ""
@@ -234,7 +235,7 @@ def recursive_copy(src_dir, dst_dir, connection, token, sync_delete=False):
                             create_directory(connection, token, trash_dir)
                         move_item(connection, token, dst_dir, trash_dir, item_name)
 
-                    if sync_delete_action == 'delete':
+                    if sync_delete_action == "delete":
                         directory_remove(connection, token, dst_dir, item_name)
 
     # 开始复制文件操作
@@ -248,7 +249,7 @@ def recursive_copy(src_dir, dst_dir, connection, token, sync_delete=False):
             if not is_path_exists(connection, token, dst_item_path):
                 create_directory(connection, token, dst_item_path)
             else:
-                print(f'文件夹【{dst_item_path}】已存在，跳过创建')
+                print(f"文件夹【{dst_item_path}】已存在，跳过创建")
 
             # 递归复制文件夹
             recursive_copy(item_path, dst_item_path, connection, token, sync_delete)
@@ -259,9 +260,9 @@ def recursive_copy(src_dir, dst_dir, connection, token, sync_delete=False):
                 src_size = item["size"]
                 dst_size = is_directory_size(connection, token, dst_item_path)
                 if src_size == dst_size:
-                    print(f'文件【{item_name}】已存在，跳过复制')
+                    print(f"文件【{item_name}】已存在，跳过复制")
                 else:
-                    print(f'文件【{item_name}】文件存在变更，删除文件')
+                    print(f"文件【{item_name}】文件存在变更，删除文件")
                     directory_remove(connection, token, dst_dir, item_name)
                     copy_item(connection, token, src_dir, dst_dir, item_name)
 
@@ -280,18 +281,21 @@ def main():
         # 执行需要使用dir_pairs的代码
         # 例如，打印dir_pairs的值
         # print(dir_pairs)
-        data_list = dir_pairs.split(';')
+        data_list = dir_pairs.split(";")
         for item in data_list:
-            pair = item.split(':')
-            if len(pair) == 2:
-                src_dir, dst_dir = pair[0], pair[1]
-                if not is_path_exists(conn, token, dst_dir):
-                    create_directory(conn, token, dst_dir)
-                print(f"【{src_dir}】---->【 {dst_dir}】")
-                print(f"同步源目录: {src_dir}, 到目标目录: {dst_dir}")
-                recursive_copy(src_dir, dst_dir, conn, token, sync_delete)
-            else:
-                print(f"源目录或目标目录不存在: {item}")
+            pair = item.split(":")
+            try:
+                if len(pair) == 2:
+                    src_dir, dst_dir = pair[0], pair[1]
+                    if not is_path_exists(conn, token, dst_dir):
+                        create_directory(conn, token, dst_dir)
+                    print(f"【{src_dir}】---->【 {dst_dir}】")
+                    print(f"同步源目录: {src_dir}, 到目标目录: {dst_dir}")
+                    recursive_copy(src_dir, dst_dir, conn, token, sync_delete)
+                else:
+                    print(f"源目录或目标目录不存在: {item}")
+            except Exception as e:
+                print(f"同步目录【{item}】失败: {e}")
 
     conn.close()
     print(f"同步任务运行结束 {datetime.now()}")
